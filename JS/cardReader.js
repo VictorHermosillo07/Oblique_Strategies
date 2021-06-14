593470be204f9ca3828d5d2b45a4405a9bfc8aa1
Victor Hermosillo <victorhermosillo.dp@gmail.com>
2021-06-14 01:00:24 -0600

    Adding Functionality

---
+++ JS/cardReader.js
@@ -0,0 +1,5 @@
+let phrase = document.getElementById('phrase')
+
+function choicePhrase(){
+    phrase.innerHTML = data[Math.floor(Math.random() * 127)].text
+}
---
+++ JS/data.js
@@ -0,0 +1,510 @@
+var data =[
+        {
+            "id": 1,
+            "text": "Remove specifics and convert to ambiguities"
+        },
+        {
+            "id": 2,
+            "text": "Don't be frightened of cliches"
+        },
+        {
+            "id": 3,
+            "text": "What is the reality of the situation?"
+        },
+        {
+            "id": 4,
+            "text": "Are there sections?  Consider transitions"
+        },
+        {
+            "id": 5,
+            "text": "Turn it upside down"
+        },
+        {
+            "id": 6,
+            "text": "Think of the radio"
+        },
+        {
+            "id": 7,
+            "text": "Allow an easement (an easement is the abandonment of a stricture)"
+        },
+        {
+            "id": 8,
+            "text": "Simple subtraction"
+        },
+        {
+            "id": 9,
+            "text": "Be dirty"
+        },
+        {
+            "id": 10,
+            "text": "Go slowly all the way round the outside"
+        },
+        {
+            "id": 11,
+            "text": "A line has two sides"
+        },
+        {
+            "id": 12,
+            "text": "Make an exhaustive list of everything you might do & do the last thing on the list"
+        },
+        {
+            "id": 13,
+            "text": "Into the impossible"
+        },
+        {
+            "id": 14,
+            "text": "Towards the insignificant"
+        },
+        {
+            "id": 15,
+            "text": "Ask people to work against their better judgement"
+        },
+        {
+            "id": 16,
+            "text": "Take away the elements in order of apparent non-importance"
+        },
+        {
+            "id": 17,
+            "text": "Infinitesimal gradations"
+        },
+        {
+            "id": 18,
+            "text": "Change instrument roles"
+        },
+        {
+            "id": 19,
+            "text": "Accretion"
+        },
+        {
+            "id": 20,
+            "text": "Disconnect from desire"
+        },
+        {
+            "id": 21,
+            "text": "Emphasize repetitions"
+        },
+        {
+            "id": 22,
+            "text": "Faced with a choice, do both (given by Dieter Rot)"
+        },
+        {
+            "id": 23,
+            "text": "Children -speaking -singing"
+        },
+        {
+            "id": 24,
+            "text": "Lost in useless territory"
+        },
+        {
+            "id": 25,
+            "text": "A very small object. Its center"
+        },
+        {
+            "id": 26,
+            "text": "Dont be afraid of things because they're easy to do"
+        },
+        {
+            "id": 27,
+            "text": "Dont be frightened to display your talents"
+        },
+        {
+            "id": 28,
+            "text": "Breathe more deeply"
+        },
+        {
+            "id": 29,
+            "text": "Honor thy error as a hidden intention"
+        },
+        {
+            "id": 30,
+            "text": "What are the sections sections of? Imagine a caterpillar moving"
+        },
+        {
+            "id": 31,
+            "text": "Only one element of each kind"
+        },
+        {
+            "id": 32,
+            "text": "Is there something missing"
+        },
+        {
+            "id": 33,
+            "text": "Use 'unqualified' people"
+        },
+        {
+            "id": 34,
+            "text": "How would you have done it?"
+        },
+        {
+            "id": 35,
+            "text": "Emphasize differences"
+        },
+        {
+            "id": 36,
+            "text": "Do nothing for as long as possible"
+        },
+        {
+            "id": 37,
+            "text": "Bridges -build -burn"
+        },
+        {
+            "id": 38,
+            "text": "Always give yourself credit for having more than personality (given by Arto Lindsay)"
+        },
+        {
+            "id": 39,
+            "text": "You don't have to be ashamed of using your own ideas"
+        },
+        {
+            "id": 40,
+            "text": "Tidy up"
+        },
+        {
+            "id": 41,
+            "text": "Do the words need changing?"
+        },
+        {
+            "id": 42,
+            "text": "Ask your body"
+        },
+        {
+            "id": 43,
+            "text": "Tape your mouth (given by Ritva Saarikko)"
+        },
+        {
+            "id": 44,
+            "text": "Water"
+        },
+        {
+            "id": 45,
+            "text": "Simply a matter of work"
+        },
+        {
+            "id": 46,
+            "text": "Make a sudden, destructive unpredictable action; incorporate"
+        },
+        {
+            "id": 47,
+            "text": "Consult other sources -promising -unpromising"
+        },
+        {
+            "id": 48,
+            "text": "Use an unacceptable color"
+        },
+        {
+            "id": 49,
+            "text": "Humanize something free of error"
+        },
+        {
+            "id": 50,
+            "text": "Use filters"
+        },
+        {
+            "id": 51,
+            "text": "Fill every beat with something"
+        },
+        {
+            "id": 52,
+            "text": "Discard an axiom"
+        },
+        {
+            "id": 53,
+            "text": "Not building a wall but making a brick"
+        },
+        {
+            "id": 54,
+            "text": "What wouldn't you do?"
+        },
+        {
+            "id": 55,
+            "text": "Lowest common denominator"
+        },
+        {
+            "id": 56,
+            "text": "Decorate, decorate"
+        },
+        {
+            "id": 57,
+            "text": "Balance the consistency principle with the inconsistency principle"
+        },
+        {
+            "id": 58,
+            "text": "Get your neck massaged"
+        },
+        {
+            "id": 59,
+            "text": "Listen to the quiet voice"
+        },
+        {
+            "id": 60,
+            "text": "Do the washing up"
+        },
+        {
+            "id": 61,
+            "text": "Is it finished?"
+        },
+        {
+            "id": 62,
+            "text": "Put in earplugs"
+        },
+        {
+            "id": 63,
+            "text": "Reevaluation (a warm feeling)"
+        },
+        {
+            "id": 64,
+            "text": "Give the name away"
+        },
+        {
+            "id": 65,
+            "text": "Intentions -nobility of -humility of -credibility of"
+        },
+        {
+            "id": 66,
+            "text": "Abandon normal instruments"
+        },
+        {
+            "id": 67,
+            "text": "Use fewer notes"
+        },
+        {
+            "id": 68,
+            "text": "Repetition is a form of change"
+        },
+        {
+            "id": 69,
+            "text": "Give way to your worst impulse"
+        },
+        {
+            "id": 70,
+            "text": "Reverse"
+        },
+        {
+            "id": 71,
+            "text": "Trust in the you of now"
+        },
+        {
+            "id": 72,
+            "text": "Imagine the piece as a set of disconnected events"
+        },
+        {
+            "id": 73,
+            "text": "What would your closest friend do?"
+        },
+        {
+            "id": 74,
+            "text": "Distorting time"
+        },
+        {
+            "id": 75,
+            "text": "Make a blank valuable by putting it in an exquisite frame"
+        },
+        {
+            "id": 76,
+            "text": "Feed the recording back out of the medium"
+        },
+        {
+            "id": 77,
+            "text": "Convert a melodic element into a rhythmic element"
+        },
+        {
+            "id": 78,
+            "text": "The most important thing is the thing most easily forgotten"
+        },
+        {
+            "id": 79,
+            "text": "[blank white card]"
+        },
+        {
+            "id": 80,
+            "text": "Ghost echoes"
+        },
+        {
+            "id": 81,
+            "text": "You can only make one dot at a time"
+        },
+        {
+            "id": 82,
+            "text": "Just carry on"
+        },
+        {
+            "id": 83,
+            "text": "(Organic) machinery"
+        },
+        {
+            "id": 84,
+            "text": "The inconsistency principle"
+        },
+        {
+            "id": 85,
+            "text": "Don't break the silence"
+        },
+        {
+            "id": 86,
+            "text": "Idiot glee (?)"
+        },
+        {
+            "id": 87,
+            "text": "Discover the recipes you are using and abandon them"
+        },
+        {
+            "id": 88,
+            "text": "Cascades"
+        },
+        {
+            "id": 89,
+            "text": "Courage!"
+        },
+        {
+            "id": 90,
+            "text": "Spectrum analysis"
+        },
+        {
+            "id": 91,
+            "text": "What mistakes did you make last time?"
+        },
+        {
+            "id": 92,
+            "text": "Consider different fading systems"
+        },
+        {
+            "id": 93,
+            "text": "Mute and continue"
+        },
+        {
+            "id": 94,
+            "text": "Be extravagant"
+        },
+        {
+            "id": 95,
+            "text": "It is quite possible (after all)"
+        },
+        {
+            "id": 96,
+            "text": "What are you really thinking about just now?"
+        },
+        {
+            "id": 97,
+            "text": "Don't stress on thing more than another [sic]"
+        },
+        {
+            "id": 98,
+            "text": "State the problem in words as clearly as possible"
+        },
+        {
+            "id": 99,
+            "text": "Assemble some of the elements in a group and treat the group "
+        },
+        {
+            "id": 100,
+            "text": "You are an engineer"
+        },
+        {
+            "id": 101,
+            "text": "Remove ambiguities and convert to specifics"
+        },
+        {
+            "id": 102,
+            "text": "Look at the order in which you do things"
+        },
+        {
+            "id": 103,
+            "text": "Go outside. Shut the door."
+        },
+        {
+            "id": 104,
+            "text": "Disciplined self-indulgence"
+        },
+        {
+            "id": 105,
+            "text": "Do we need holes?"
+        },
+        {
+            "id": 106,
+            "text": "Cluster analysis"
+        },
+        {
+            "id": 107,
+            "text": "Always first steps"
+        },
+        {
+            "id": 108,
+            "text": "Cut a vital conenction"
+        },
+        {
+            "id": 109,
+            "text": "Do something boring"
+        },
+        {
+            "id": 110,
+            "text": "Define an area as `safe' and use it as an anchor"
+        },
+        {
+            "id": 111,
+            "text": "Is the information correct?"
+        },
+        {
+            "id": 112,
+            "text": "Overtly resist change"
+        },
+        {
+            "id": 113,
+            "text": "Question the heroic approach"
+        },
+        {
+            "id": 114,
+            "text": "Accept advice"
+        },
+        {
+            "id": 115,
+            "text": "Twist the spine"
+        },
+        {
+            "id": 116,
+            "text": "Work at a different speed"
+        },
+        {
+            "id": 117,
+            "text": "Look closely at the most embarrassing details & amplify them"
+        },
+        {
+            "id": 118,
+            "text": "Mechanicalize something idiosyncratic"
+        },
+        {
+            "id": 119,
+            "text": "Emphasize the flaws"
+        },
+        {
+            "id": 120,
+            "text": "Remember those quiet evenings"
+        },
+        {
+            "id": 121,
+            "text": "Take a break"
+        },
+        {
+            "id": 122,
+            "text": "Short circuit (example; a man eating peas with the idea that they will improve  his virility shovels them straight into his lap)"
+        },
+        {
+            "id": 123,
+            "text": "Left channel, right channel, center channel"
+        },
+        {
+            "id": 124,
+            "text": "Use an old idea"
+        },
+        {
+            "id": 125,
+            "text": "Destroy  -nothing  -the most important thing"
+        },
+        {
+            "id": 126,
+            "text": "Change nothing and continue with immaculate consistency"
+        },
+        {
+            "id": 127,
+            "text": "The tape is now the music"
+        }
+    ];
